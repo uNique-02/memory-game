5ea8d437e35da9661a98f990809f41553923e28c
kimz <92148017+uNique-02@users.noreply.github.com>
2026-04-08 22:27:59 +0800

    Modified UI and fixed rate-limit

--- server/server.js
+++ server/server.js
@@ -110,9 +110,9 @@ if (process.env.NODE_ENV === "production") {
 
   app.use(express.static(frontendDistPath));
 
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(frontendDistPath, "index.html"));
-  });
+  app.use((req, res) => {
+  res.sendFile(path.join(frontendDistPath, "index.html"));
+});
 }
 
 /**
